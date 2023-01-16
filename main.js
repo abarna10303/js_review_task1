b2e9f785fc4399f26f55ae8650144b69bccbeba0
abarna10303 <abarnas361@gmail.com>
2023-01-16 19:57:20 +0530

    Roller host

---
+++ main.js
@@ -0,0 +1,21 @@
+let age=parseInt(prompt("Enter the Age"));
+let parentpermission=true;
+if(age<16)
+{
+    if(parentpermission==true)
+    {
+        console.log("Eligible for roller host");
+    }
+    else
+    {
+        console.log("Not Eligible for roller host");
+    }
+}
+else if(age>=16 && age<=60)
+{
+    console.log("Eligible for roller host");
+}
+else
+{
+    console.log("Not Eligible for roller host");
+}
